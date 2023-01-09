d9d18f19853c4e2a4e1cd095e2ec77accfd69567
dreamapaul <dreamapaulv@gmail.com>
2023-01-09 18:46:15 +0530

    move main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hotelapp/UI/main.dart';
+import 'package:hotelapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
